50ff1c0ae77f85b7cc39543ad573527956794c94
Kyla Korkowski <kekorkowski@gmail.com>
2023-03-30 15:33:51 -1000

    Fianl commit

--- api/server.js
+++ api/server.js
@@ -1,25 +1,24 @@
 const express = require('express');
 const server = express();
-
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
-
 server.use(express.json());
 
-const {  } = require('./projects/projects-middleware');
 const projectsRouter = require('./projects/projects-router');
 server.use('/api/projects', projectsRouter);
 
-const {  } = require('./actions/actions-middlware');
 const actionsRouter = require('./actions/actions-router');
 server.use('/api/actions', actionsRouter);
 
-server.get('/api', (req, res) => {
-    res.status(200).json({
-        message: 'it bout to get krunk up in dis bitch.'
-    })
-})
+server.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({
+    message: 'Internal server error'
+  });
+});
+
+server.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Server is up and running!'
+  });
+});
 
 module.exports = server;
